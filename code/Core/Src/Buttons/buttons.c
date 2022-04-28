dc9def618ad7efea082687a228631be2683d5b32
luk6xff <lukasz.uszko@gmail.com>
2022-04-29 00:37:02 +0200

    Enable FreeRtos, Draft of buttons task

---
+++ code/Core/Src/Buttons/buttons.c
@@ -0,0 +1,148 @@
+/*
+ * buttons.c
+ *
+ *  Created on: Apr 27, 2022
+ *      Author: luk6xff
+ */
+
+#include "buttons.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+//------------------------------------------------------------------------------
+struct ButtonStatus
+{
+	ButtonState current_state;
+	ButtonState previous_state;
+
+	void (*button_released)(void);
+	void (*button_pressed_short)(void);
+	void (*button_pressed_long)(void);
+};
+
+
+//------------------------------------------------------------------------------
+struct ButtonConfig
+{
+	uint16_t pin;
+	GPIO_TypeDef *port;
+	GPIO_PinState pressed_pin_state;
+
+};
+
+//------------------------------------------------------------------------------
+const struct ButtonConfig buttons_cfg[BUTTONS_NUMBER] =
+{
+	{ HOTAIR_ON_SW_Pin, 		HOTAIR_ON_SW_GPIO_Port,			GPIO_PIN_RESET },
+	{ HOTAIR_TEMP_UP_SW_Pin, 	HOTAIR_TEMP_UP_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ HOTAIR_TEMP_DOWN_SW_Pin, 	HOTAIR_TEMP_DOWN_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ HOTAIR_FAN_UP_SW_Pin, 	HOTAIR_FAN_UP_SW_GPIO_Port,		GPIO_PIN_RESET },
+	{ HOTAIR_FAN_DOWN_SW_Pin, 	HOTAIR_FAN_DOWN_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ SOLDER_SELECT_T12_SW_Pin, SOLDER_SELECT_T12_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ SOLDER_SELECT_WEP_SW_Pin, SOLDER_SELECT_WEP_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ SOLDER_TEMP_UP_SW_Pin, 	SOLDER_TEMP_UP_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ SOLDER_TEMP_DOWN_SW_Pin, 	SOLDER_TEMP_DOWN_SW_GPIO_Port,	GPIO_PIN_RESET },
+	{ MENU_RIGHT_SW_Pin, 		MENU_RIGHT_SW_GPIO_Port,		GPIO_PIN_RESET },
+	{ MENU_OK_SW_Pin,			MENU_OK_SW_GPIO_Port,			GPIO_PIN_RESET },
+	{ MENU_LEFT_SW_Pin, 		MENU_LEFT_SW_GPIO_Port,			GPIO_PIN_RESET },
+	{ HOTAIR_REED_SW_Pin, 		HOTAIR_REED_SW_GPIO_Port,		GPIO_PIN_SET   },
+	{ IRON_T12_MOTION_SW_Pin, 	IRON_T12_MOTION_SW_GPIO_Port,	GPIO_PIN_SET   },
+};
+
+//------------------------------------------------------------------------------
+static struct ButtonStatus buttons[BUTTONS_NUMBER];
+
+//------------------------------------------------------------------------------
+void buttons_task(void *argument)
+{
+	// Last wake time
+	TickType_t last_wake_time = xTaskGetTickCount();
+	// Sampling interval
+	const TickType_t sample_interval = 50/portTICK_PERIOD_MS;
+	// Setup default state
+	for (int i = 0; i < BUTTONS_NUMBER; ++i)
+	{
+		buttons[i].current_state = BUTTON_NOT_PRESSED;
+		buttons[i].previous_state = BUTTON_NOT_PRESSED;
+	}
+
+	for(;;)
+	{
+
+		// Wait for the next cycle.
+		vTaskDelayUntil(&last_wake_time, sample_interval);
+
+		// Get the level on button pin
+		for (int i = 0; i < BUTTONS_NUMBER; ++i)
+		{
+			const GPIO_PinState pin_state  = HAL_GPIO_ReadPin(buttons_cfg[i].port, buttons_cfg[i].pin);
+
+			if (pin_state == buttons_cfg[i].pressed_pin_state ^ 1)
+			{
+				buttons[i].current_state = BUTTON_NOT_PRESSED;
+			}
+			else if ((pin_state == buttons_cfg[i].pressed_pin_state) && (buttons[i].previous_state == BUTTON_NOT_PRESSED))
+			{
+				buttons[i].current_state = BUTTON_SHORT_PRESSED;
+				// Run pressed_short cb if registered
+				if (buttons[i].button_pressed_short)
+				{
+					(*buttons[i].button_pressed_short)();
+				}
+			}
+			else if ((pin_state == buttons_cfg[i].pressed_pin_state) && \
+					 (buttons[i].previous_state == BUTTON_SHORT_PRESSED || \
+					  buttons[i].previous_state == BUTTON_LONG_PRESSED))
+			{
+				buttons[i].current_state = BUTTON_LONG_PRESSED;
+				// Run pressed_long cb if registered
+				if (buttons[i].button_pressed_long)
+				{
+					(*buttons[i].button_pressed_long)();
+				}
+			}
+			else if ((pin_state == buttons_cfg[i].pressed_pin_state ^ 1) && \
+					 (buttons[i].previous_state == BUTTON_SHORT_PRESSED || \
+					  buttons[i].previous_state == BUTTON_LONG_PRESSED))
+			{
+				buttons[i].current_state = BUTTON_NOT_PRESSED;
+				// Run released cb if registered
+				if (buttons[i].button_released)
+				{
+					(*buttons[i].button_released)();
+				}
+			}
+
+			buttons[i].previous_state = buttons[i].current_state;
+		}
+	}
+}
+
+//------------------------------------------------------------------------------
+void button_released_cb(ButtonType type, void(*cb)(void))
+{
+	buttons[(int)type].button_released = cb;
+}
+
+//------------------------------------------------------------------------------
+void button_pressed_short_cb(ButtonType type, void(*cb)(void))
+{
+	buttons[(int)type].button_pressed_short = cb;
+}
+
+//------------------------------------------------------------------------------
+void button_pressed_long_cb(ButtonType type, void(*cb)(void))
+{
+	buttons[(int)type].button_pressed_long = cb;
+}
+
+//------------------------------------------------------------------------------
+const ButtonState buttons_state()
+{
+	// LU_TODO
+	return BUTTON_NOT_PRESSED;
+}
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
---
+++ code/Core/Src/Buttons/buttons.h
@@ -0,0 +1,87 @@
+/*
+ * buttons.h
+ *
+ *  Created on: Apr 27, 2022
+ *      Author: luk6xff
+ */
+
+#ifndef SRC_BUTTONS_BUTTONS_H_
+#define SRC_BUTTONS_BUTTONS_H_
+
+#include "gpio.h"
+
+
+
+// Last time of button pressed
+extern uint32_t last_button_time;
+
+
+/**
+ * @brief Button types
+ */
+typedef enum
+{
+	// HOTAIR BUTTONS
+	BUTTON_HOTAIR_ON = 0,
+	BUTTON_HOTAIR_TEMP_UP,
+	BUTTON_HOTAIR_TEMP_DOWN,
+	BUTTON_HOTAIR_FAN_UP,
+	BUTTON_HOTAIR_FAN_DOWN,
+
+	// SOLDERING IRON BUTTONS
+	//BUTTON_SOLDER_ON, - NOT USED
+	BUTTON_SOLDER_TEMP_DOWN,
+	BUTTON_SOLDER_TEMP_UP,
+	BUTTON_SOLDER_SELECT_T12,
+	BUTTON_SOLDER_SELECT_WEP,
+
+	// MENU BUTTONS
+	BUTTON_MENU_RIGHT,
+	BUTTON_MENU_OK,
+	BUTTON_MENU_LEFT,
+
+	// SENSING SW
+	BUTTON_HOTAIR_REED,
+	BUTTON_IRON_T12_MOTION,
+
+	// Number of all supported buttons
+	// Keep it always as last
+	BUTTONS_NUMBER,
+} ButtonType;
+
+/**
+ * @brief Button state
+ */
+typedef enum
+{
+	BUTTON_NOT_PRESSED = 0,
+	BUTTON_SHORT_PRESSED,
+	BUTTON_LONG_PRESSED,
+} ButtonState;
+
+/**
+ * @brief Buttons polling task
+ */
+void buttons_task(void *argument);
+
+/**
+ * @brief Set button released callback
+ */
+void button_released_cb(ButtonType type, void(*cb)(void));
+
+/**
+ * @brief Set button pressed_short callback
+ */
+void button_pressed_short_cb(ButtonType type, void(*cb)(void));
+
+/**
+ * @brief Set button pressed_long callback
+ */
+void button_pressed_long_cb(ButtonType type, void(*cb)(void));
+
+/**
+ * @brief Returns what buttons are pressed.
+ */
+const ButtonState buttons_state();
+
+#endif /* SRC_BUTTONS_BUTTONS_H_ */
